74ef413db1b5c10bf1546854fc9f27fcd5b115fd
Anait Margaryan <nerdoweirdo@yandex.com>
2024-01-27 03:51:37 +0400

    fourth commit

--- src/test/java/ConstructorTest.java
+++ src/test/java/ConstructorTest.java
@@ -1,59 +1,30 @@
 import io.qameta.allure.junit4.DisplayName;
-import org.hamcrest.CoreMatchers;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pageobject.AuthPage;
-import pageobject.ConstructorPage;
-import pageobject.RegisterPage;
 
-import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertTrue;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ConstructorTest extends BaseTest {
-    private RegisterPage registerPage;
-    private AuthPage authPage;
-    private  ConstructorPage constructorPage;
+public class ConstructorTest extends Base {
 
-    @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win32\\chromedriver.exe");
-        driver = new ChromeDriver();
-
-        driver.get("https://stellarburgers.nomoreparties.site/register");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-        registerPage = new RegisterPage(driver);
-        registerPage.register(name, email, password);
-        driver.get("https://stellarburgers.nomoreparties.site/login");
-        authPage = new AuthPage(driver);
-        authPage.login(email, password);
-
-    }
-
-    @DisplayName("buns section")
     @Test
+    @DisplayName("going to the 'Buns' section")
     public void bunsSectionTest() {
-        constructorPage = new ConstructorPage(driver);
-
-        assertThat(constructorPage.bunsSection(), CoreMatchers.containsString("tab_tab_type_current"));
+        mainPage.clickFilling();
+        mainPage.clickBun();
+        assertTrue("went to the Buns section", mainPage.isBunActive());
     }
 
-    @DisplayName("sauce section")
     @Test
-    public void sauceSectionTest() {
-        constructorPage = new ConstructorPage(driver);
-        constructorPage.clickSauceTab();
-        assertThat(constructorPage.saucesSection(), CoreMatchers.containsString("tab_tab_type_current"));
+    @DisplayName("going to the 'Sauces' section")
+    public void saucesSectionTest() {
+        mainPage.clickSauce();
+        assertTrue("went to the Sauces section", mainPage.isSauceActive());
     }
 
-    @DisplayName("stuffing section")
     @Test
-    public void stuffingSectionTest() {
-        constructorPage = new ConstructorPage(driver);
-        constructorPage.clickFeelingsTab();
-        assertThat(constructorPage.fillingsSection(), CoreMatchers.containsString("tab_tab_type_current"));
+    @DisplayName("going to the 'Fillings' section")
+    public void saucesFillingsTest() {
+        mainPage.clickFilling();
+        assertTrue("Не зашел в Начинки", mainPage.isFillingActive());
     }
 }
